2cc9d5140b4c54c94f3cce393baeb67314c61249
dengyunxiang <1973503139@qq.com>
2023-04-24 17:02:23 +0800

    qiankun-base 页面展示修改

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,76 @@
-import React from "react";
-import { Link, BrowserRouter } from "react-router-dom";
+import React, { useState } from "react";
+import { BrowserRouter } from "react-router-dom";
+import { Layout, Menu, theme } from "antd";
+import HeadMenuItemsList from "./menu/menu-item-head";
 import "./App.css";
 
-function App() {
+const { Header, Content } = Layout;
+
+const App: React.FC = () => {
+  // andt 颜色
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+
+  // 保存当前选择中微应用 key
+  const [menuKey, setMenuKey] = useState(0);
+
+  // 判断当前是否为主应用
+  const isInMainProject = ["/", "/qiankun-base"].includes(
+    window.location.pathname
+  );
+
   return (
     <div className="App">
-      {/* // 先做一个简单的路由跳转 */}
       <BrowserRouter>
-        <h3>
-          <Link to="/qk-micro-react">微前端React</Link>
-        </h3>
-        <h3>
-          <Link to="/qk-micro-vue">微前端Vue</Link>
-        </h3>
+        <Layout className="layout">
+          <Header>
+            <div className="logo" />
+            <Menu
+              theme="dark"
+              mode="horizontal"
+              items={HeadMenuItemsList.map((item, index) => {
+                return {
+                  key: index,
+                  label: item?.label,
+                };
+              })}
+              onSelect={(e: any) => {
+                // 保存选中的 key
+                setMenuKey(e.key);
+                // 路由跳转
+                window.history.pushState(
+                  null,
+                  "",
+                  HeadMenuItemsList[e.key].hrefPath
+                );
+              }}
+            />
+          </Header>
+
+          <Content>
+            <div
+              className="site-layout-content"
+              style={{ background: colorBgContainer }}
+            >
+              {/* 根据路径判断当前是否在主应用中 */}
+              {isInMainProject ? (
+                <div
+                  style={{
+                    fontSize: "30px",
+                  }}
+                >
+                  主应用展示页面
+                </div>
+              ) : (
+                <div id={HeadMenuItemsList[menuKey]?.id} /> // id 要与 registerMicroApps 当中的 container 保持一致
+              )}
+            </div>
+          </Content>
+        </Layout>
       </BrowserRouter>
-      {/* id 要与 registerMicroApps 当中的 container 对齐 */}
-      {/* react子应用挂载容器 id=qk-micro-react */}
-      <div id="qk-micro-react" />
-      {/* vue子应用挂在容器 id=qk-micro-vue  */}
-      <div id="qk-micro-vue" />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/menu/menu-item-head.ts
@@ -0,0 +1,14 @@
+const HeadMenuItemsList = [
+  {
+    id: "qk-micro-react",
+    label: "微前端React",
+    hrefPath: "/qk-micro-react",
+  },
+  {
+    id: "qk-micro-vue",
+    label: "微前端Vue",
+    hrefPath: "/qk-micro-vue",
+  },
+];
+
+export default HeadMenuItemsList;
