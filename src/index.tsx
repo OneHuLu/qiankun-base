7b1bdbe05b38092f31e056c4e6478e219f7ae069
dengyunxiang <1973503139@qq.com>
2023-04-24 15:10:46 +0800

    微应用地址替换

--- src/index.tsx
+++ src/index.tsx
@@ -6,24 +6,26 @@ import reportWebVitals from "./reportWebVitals";
 // 引入配置函数
 import { registerMicroApps, start } from "qiankun";
 
-//配置两个子应用 
+//配置两个子应用
 registerMicroApps([
   {
     // 子应用名字
-    name: 'qk-micro-react',
+    name: "qk-micro-react",
     // 子应用容器id 需要在父应用中创建相关元素
-    container: '#qk-micro-react',
+    container: "#qk-micro-react",
     // 子应用进入地址
-    entry: 'http://localhost:3011',
+    // entry: 'http://localhost:3011', // 本地启用
+    entry: "https://onehulu.github.io/qiankun-mirco-react/", //github
     // 激活子应用的路由
-    activeRule: '/qk-micro-react',
+    activeRule: "/qk-micro-react",
   },
   {
-    name: 'qk-micro-vue',
-    container: '#qk-micro-vue',
-    entry: 'http://localhost:3012/',
-    activeRule: '/qk-micro-vue',
-  }
+    name: "qk-micro-vue",
+    container: "#qk-micro-vue",
+    // entry: "http://localhost:3012/",// 本地启用
+    entry: "https://onehulu.github.io/qiankun-mirco-vue/", //github
+    activeRule: "/qk-micro-vue",
+  },
 ]);
 
 //启动 qiankun
@@ -31,8 +33,8 @@ start({
   sandbox: {
     // 样式隔离特性
     experimentalStyleIsolation: true,
-  }
-})
+  },
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
